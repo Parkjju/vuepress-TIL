da285ad25da29d6cde0f710d9c42a7ee9013ab02
Parkjju <rudwns3927@gmail.com>
2022-04-03 15:53:34 +0900

    Add: GA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,6 +33,21 @@ module.exports = {
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     ],
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-F5R64LJ7MV',
+        // GA에서 발급받은 코드
+      },
+    ],
+    [
+      'script',
+      {},
+      [
+        "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-F5R64LJ7MV');",
+      ],
+    ],
   ],
 
   /**
